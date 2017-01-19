513fcd7257043b2ee35d94da5405bea406e7be75
Matt <mattdraft@gmail.com>
2017-01-19 00:21:24 -0600

    Bonus Complete

---
+++ bonus/hello1.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // cout and more
+#include <string>   // Strings
+using namespace std;
+
+
+//cin takes input
+
+int main(){
+
+	string Name;
+	cout << "Please enter your Name: " << endl;
+	getline(cin, Name);
+	cout << "Hello, " << Name << "!" << endl;
+
+
+}
+
